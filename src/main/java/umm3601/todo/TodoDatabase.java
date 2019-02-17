5898bd61e399502bbf7296339f0dff351665f76b
nkfoss <nathankfoss@gmail.com>
2019-02-17 14:28:10 -0600

    Add database functionality to handle status query params
    The filterTodosByStatus method converts the status parameter into a
    boolean value, since that is the type used for status in the todo
    JSON object.

--- src/main/java/umm3601/todo/TodoDatabase.java
+++ src/main/java/umm3601/todo/TodoDatabase.java
@@ -1,9 +1,9 @@
 package umm3601.todo;
 
 import com.google.gson.Gson;
-
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Map;
 
 
@@ -29,9 +29,31 @@ public class TodoDatabase {
   public todo[] listTodos(Map<String, String[]> queryParams) {
     todo[] filteredTodos = allTodos;
 
-    // Maybe add some filtering later, see the mirrored Database.java class
+    // Filter age if defined
+    if (queryParams.containsKey("status")) {
+      String targetStatus = queryParams.get("status")[0];
+      filteredTodos = filterTodosByStatus(filteredTodos, targetStatus);
+    }
 
     return filteredTodos;
+  }
+
+  /** Gets an array of todos filtered by their status
+   *
+   * @param todos an array of all the todos
+   * @param targetStatus String that is either 'complete' or 'incomplete' and triggers a conditional
+   *                     that checks a todo's status for true or false.
+   * @return An array of todos that have been filtered
+   */
+  public todo[] filterTodosByStatus (todo[] todos, String targetStatus) {
+    Boolean boolStatus;
+    if (targetStatus.equals("complete")) {
+      return Arrays.stream(todos).filter(x -> x.status == true).toArray(todo[]::new);
+    }
+    else {
+      return Arrays.stream(todos).filter(x -> x.status == false).toArray(todo[]::new);
+    }
+
   }
 
 
