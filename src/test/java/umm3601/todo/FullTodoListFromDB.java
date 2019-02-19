ffd0c966e4104fc456d56a4fa9e010a73f226e05
nkfoss <fossx148@morris.umn.edu>
2019-02-18 19:06:40 -0600

    Create JUnit test to test full todo list

--- src/test/java/umm3601/todo/FullTodoListFromDB.java
+++ src/test/java/umm3601/todo/FullTodoListFromDB.java
@@ -1,32 +1,32 @@
-//package umm3601.todo;
-//
-//import org.junit.Test;
-//
-//import java.io.IOException;
-//import java.util.HashMap;
-//
-//import static org.junit.Assert.assertEquals;
-//
-///**
-// * Tests umm3601.user.Database listUser functionality
-// */
-//public class FullUserListFromDB {
-//
-//  @Test
-//  public void totalUserCount() throws IOException {
-//    TodoDatabase tdb = new TodoDatabase("src/main/data/todos.json");
-//    todo[] allTodos = tdb.listTodos(new HashMap<>());
-//    assertEquals("Incorrect total number of todos", 300, allTodos.length);
-//  }
-//
-//  @Test
-//  public void firstTodoInFullList() throws IOException {
-//    TodoDatabase tdb = new TodoDatabase("src/main/data/todos.json");
-//    todo[] allUsers = tdb.listTodos(new HashMap<>());
-//    todo firstTodo = allUsers[0];
-//    assertEquals("Incorrect name", "Connie Stewart", firstUser.name);
-//    assertEquals("Incorrect age", 25, firstUser.age);
-//    assertEquals("Incorrect company", "OHMNET", firstUser.company);
-//    assertEquals("Incorrect e-mail", "conniestewart@ohmnet.com", firstUser.email);
-//  }
-//}
+package umm3601.todo;
+
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Tests umm3601.user.Database listUser functionality
+ */
+public class FullTodoListFromDB {
+
+  @Test
+  public void totalUserCount() throws IOException {
+    TodoDatabase tdb = new TodoDatabase("src/main/data/todos.json");
+    todo[] allTodos = tdb.listTodos(new HashMap<>());
+    assertEquals("Incorrect total number of todos", 300, allTodos.length);
+  }
+
+  @Test
+  public void firstTodoInFullList() throws IOException {
+    TodoDatabase tdb = new TodoDatabase("src/main/data/todos.json");
+    todo[] allTodos = tdb.listTodos(new HashMap<>());
+    todo firstTodo = allTodos[0];
+    assertEquals("Incorrect owner", "Blanche", firstTodo.owner);
+    assertEquals("Incorrect status", true, firstTodo.status);
+    assertEquals("Incorrect category", "software design", firstTodo.category);
+    assertEquals("Incorrect id", "58895985a22c04e761776d54", firstTodo._id);
+  }
+}
