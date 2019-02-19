beb97ae89c887777d0570f63108aa8993c24fbf6
UnicornWizard375 <drumm040@morris.umn.edu>
2019-02-18 18:56:29 -0600

    Work on the tst file for filtering todos

--- src/test/java/umm3601/user/TestFilteringTodos.java
+++ src/test/java/umm3601/user/TestFilteringTodos.java
@@ -4,7 +4,8 @@ import umm3601.todo.*;
   import org.junit.Test;
   import java.io.FileReader;
   import java.io.IOException;
-  import java.util.Map;
+import java.util.HashMap;
+import java.util.Map;
   import static org.junit.Assert.assertTrue;
   import static junit.framework.TestCase.assertEquals;
 
@@ -14,14 +15,18 @@ public class TestFilteringTodos {
 
 
   @Test
-  public void testthing(todo[] tempTodos) {
+  public void testthing() throws IOException {
 
+      Database db = new Database("src/main/data/todos.json");
+      Map<String, String[]> queryParams = new HashMap<>();
 
-    assertTrue("hi",true);
+      queryParams.put("number", new String[]{"10"});
+      //todo[] listOfTen = db.listTodos(queryParams);
+      //assertEquals("Incorrect number of users with age 27", 10, listOfTen.length);
 
+      queryParams.put("age", new String[]{"33"});
+      User[] age33Users = db.listUsers(queryParams);
+      assertEquals("Incorrect number of users with age 33", 1, age33Users.length);
+    }
   }
 
-
-
-
-}
