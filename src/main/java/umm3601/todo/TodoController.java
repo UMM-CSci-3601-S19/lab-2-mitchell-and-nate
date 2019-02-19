fa8668fcb068f737dbab3fa11fe8bee8540b21ba
nkfoss <fossx148@morris.umn.edu>
2019-02-18 22:38:06 -0600

    Remove comments from instructor

--- src/main/java/umm3601/todo/TodoController.java
+++ src/main/java/umm3601/todo/TodoController.java
@@ -7,23 +7,12 @@ import spark.Response;
 
 import static umm3601.Util.*;
 
-/**
- * Controller that manages requests for info about users.
- */
 public class TodoController {
 
   private final Gson gson;
   private TodoDatabase todoDatabase;
 
-  /**
-   * Construct a controller for users.
-   * <p>
-   * This loads the "database" of user info from a JSON file and
-   * stores that internally so that (subsets of) users can be returned
-   * in response to requests.
-   *
-   * @param todoDatabase the database containing user data
-   */
+
   public TodoController(TodoDatabase todoDatabase) {
     gson = new Gson();
     this.todoDatabase = todoDatabase;
